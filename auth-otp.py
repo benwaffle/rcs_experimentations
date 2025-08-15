d62116d7ed0f8362b57ac7f740b2d6a3fee6065f
Pierre-Hugues Husson <phh@phh.me>
2025-08-15 22:40:25 +0200

    auth: Auto-detect carrier from imsi, prevent running if there is already a token

--- auth-otp.py
+++ auth-otp.py
@@ -1,12 +1,36 @@
 import requests
 import uuid
 import sys
-from secrets import imsi, imei, msisdn
+from my_secrets import imsi, imei, msisdn
+from pathlib import Path
 
 session = requests.Session()
 
 #config_url = 'https://config.rcs.mnc010.mcc208.pub.3gppnetwork.org/'
-config_url = 'https://config.rcs.mnc010.mcc208.jibecloud.net/'
+mcc = imsi[:3]
+config_url = f'https://config.rcs.mnc010.mcc{mcc}.jibecloud.net/'
+urls = []
+
+p = Path("wap-provisioningdoc.xml")
+if p.exists():
+    print("Looks like you're already authed. If you're sure, delete wap-provisioningdoc.xml")
+    sys.exit(0)
+
+for url_format in ['https://config.rcs.mnc{mnc}.mcc{mcc}.pub.3gppnetwork.org/', 'https://config.rcs.mnc{mnc}.mcc{mcc}.jibecloud.net/']:
+    for mnc in [imsi[3:5], '0' + imsi[3:5], imsi[3:6]]:
+        try:
+            url = url_format.format(mcc = mcc, mnc = mnc)
+            req = requests.head(url)
+            # No matter the return code let's consider it a success
+            # Jibe returns 405 - method not recognized
+            config_url = url
+            break
+        except:
+            pass
+
+print("Successful url", url)
+sys.exit(0)
+
 
 common_headers = {
     'client_channel': 'PUBLIC',
@@ -70,3 +94,5 @@ params_3 = {
 response_3 = session.get(config_url, headers=common_headers, params=params_3)
 print(response_3)
 print(response_3.text)
+with open('wap-provisioningdoc.xml', 'w') as f:
+    f.write(response_3.text)
