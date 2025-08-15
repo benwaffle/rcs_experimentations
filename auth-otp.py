b00813d4f1cfc04678680dea6ba58a3e2088dd6c
Pierre-Hugues Husson <phh@phh.me>
2025-08-15 22:25:36 +0200

    Initial commit

---
+++ auth-otp.py
@@ -0,0 +1,72 @@
+import requests
+import uuid
+import sys
+from secrets import imsi, imei, msisdn
+
+session = requests.Session()
+
+#config_url = 'https://config.rcs.mnc010.mcc208.pub.3gppnetwork.org/'
+config_url = 'https://config.rcs.mnc010.mcc208.jibecloud.net/'
+
+common_headers = {
+    'client_channel': 'PUBLIC',
+    'User-Agent': 'IM-client/OMA1.0 Google/Pixel_3-14 Goog/messages.android_20240603_01_rc01',
+    'Accept-Language': 'fr-FR',
+    'Connection': 'Keep-Alive'
+}
+
+params_1 = {
+    'vers': '0',
+    'rcs_state': '0',
+    'IMSI': imsi,
+    'IMEI': imei,
+    'terminal_model': 'Pixel 3',
+    'terminal_vendor': 'Goog',
+    'terminal_sw_version': '12',
+    'client_vendor': 'Goog',
+    'client_version': '20240603-01.01',
+    'rcs_profile': 'UP_T',
+    'rcs_version': '5.1B',
+    'token': '',
+    'SMS_port': '0'
+}
+
+response_1 = session.get(config_url, headers=common_headers, params=params_1)
+
+print(response_1)
+print(response_1.text)
+print(response_1.cookies)
+
+
+params_2 = params_1.copy()
+params_2.update({
+    'msisdn': msisdn,
+})
+
+headers_2 = common_headers.copy()
+headers_2.update({
+    'msisdn_source': 'msisdn_source_sim'
+})
+
+response_2 = session.get(config_url, headers=headers_2, params=params_2)
+print(response_2)
+print(response_2.text)
+print(response_2.__dict__)
+
+if response_2.status_code != 200 and response_2.status_code != 511:
+    print('Error: ', response_2.status_code)
+    sys.exit(0)
+
+cookies = response_2.cookies
+print(cookies)
+
+otp = input("Please enter the OTP: ")
+
+params_3 = params_2.copy()
+params_3 = {
+    'OTP': otp
+}
+
+response_3 = session.get(config_url, headers=common_headers, params=params_3)
+print(response_3)
+print(response_3.text)
